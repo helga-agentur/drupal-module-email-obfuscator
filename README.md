0f3521ed6cf645733c5a5a85e87d2d7b38679096
Hudson Muff <hudson@joinbox.com>
2023-02-28 17:31:24 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# drupal-module-email-obfuscator
-Drupal Email Obfuscator Module
+# Drupal Module Email Obfuscator
+
+Drupal Email Obfuscator Module is used to search everywhere and obfuscate all emails within Drupal using a Middleware.
+The following is done to the emails:
+- `mailto:` links are removed with a regex and replaced with an onclick
+- Plaintext emails are reversed with CSS and made readable (ltr -> rtl)
---
+++ src/EmailObfuscatorMiddleware.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\email_obfuscator;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * EmailObfuscatorMiddleware middleware.
+ */
+class EmailObfuscatorMiddleware implements HttpKernelInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The kernel.
+   *
+   * @var \Symfony\Component\HttpKernel\HttpKernelInterface
+   */
+  protected $httpKernel;
+
+  /**
+   * Constructs the EmailObfuscatorMiddleware object.
+   *
+   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $http_kernel
+   *   The decorated kernel.
+   */
+  public function __construct(HttpKernelInterface $http_kernel) {
+    $this->httpKernel = $http_kernel;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
+    $response = $this->httpKernel->handle($request, $type, $catch);
+
+    try {
+      if ($content = $response->getContent()) {
+        if ($obfuscateEmails = $this->obfuscateEmails($content)) {
+          $response->setContent($obfuscateEmails);
+        }
+      }
+    } catch (\Exception $e) {
+      \Drupal::logger('email_obfuscator')->error($e->getMessage());
+    }
+
+    return $response;
+  }
+
+  /**
+   * Remove emails from mailto links and reverse the emails so bots can't read them - hopefully.
+   * https://web.archive.org/web/20180908103745/http://techblog.tilllate.com/2008/07/20/ten-methods-to-obfuscate-e-mail-addresses-compared/
+   *
+   * @param string $content
+   *
+   * @return string The processed string
+   * @throws \Exception
+   */
+  private function obfuscateEmails(string $content): string {
+    // remove mailto links and replace with onclick rereverse method
+    $mailtoRegex = '/(href=)"mailto:[^"]+"/';
+
+    $obfuscatedContent = preg_replace(
+      $mailtoRegex,
+      "$1\"#\" onclick='this.href=`mailto:` + this.querySelector(`span`).textContent.split(``).reverse().join(``)'",
+      $content
+    );
+
+    if (!$obfuscatedContent) {
+      throw new \Exception('Removing mailtos with regex and adding onclick to email links failed.');
+    }
+
+    // css reverse all emails and show correctly by changing the text direction
+    $emailRegex = '/([\w\.\+\-]+@[\w\-\.]+\.[a-zA-Z]{2,})/';
+
+    return preg_replace_callback(
+      $emailRegex,
+      function ($matches) {
+        return "<span style='unicode-bidi:bidi-override;direction:rtl'>" . strrev(
+            $matches[0]
+          ) . "</span>";
+      },
+      $obfuscatedContent
+    ) ?? throw new \Exception('CSS reversing emails failed.');
+  }
+}
